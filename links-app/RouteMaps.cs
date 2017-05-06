2ba917fda85ea2d7d2dfb0f1b2abbe5cee89d7e6
Walking Disaster <michaelmeadows@gmail.com>
2017-05-06 15:13:31 -0400

    slash

--- links-app/RouteMaps.cs
+++ links-app/RouteMaps.cs
@@ -10,12 +10,12 @@ namespace links_app
         public static IDictionary<string, string> Routes = new Dictionary<string, string>
         {
             {"/github", $"{GitHubRoot}"},
-            {"/blog", $"{HomePageRoot}/blog"},
-            {"/proj/edux", $"{GitHubRoot}/edux/tree/stirtrek2017"},
+            {"/blog", $"{HomePageRoot}blog"},
+            {"/proj/edux", $"{GitHubRoot}edux/tree/stirtrek2017"},
 
-            {"/st2017", $"{HomePageRoot}/single-post/presentations/2017/stirtrek-resources"},
+            {"/st2017", $"{HomePageRoot}single-post/presentations/2017/stirtrek-resources"},
             {"/pres/2017/stirtrek/slides", "https://docs.google.com/presentation/d/1T0iYRul6NdwM6GpE-jDuQBD13kFspxiXXXlwDQKOwpM/edit?usp=sharing"},
-            {"/pres/2017/stirtrek/edux", $"{GitHubRoot}/edux/tree/stirtrek2017"},
+            {"/pres/2017/stirtrek/edux", $"{GitHubRoot}edux/tree/stirtrek2017"},
             {"/plus", "https://plus.google.com/u/0/+MichaelMeadowsAndYourMom" }
         };
     }
