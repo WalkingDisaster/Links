91ccb85744d1636ece32c0e2ed4e2b46f26aabed
Michael Meadows <michaelmeadows@gmail.com>
2018-09-03 16:39:09 -0400

    Update RouteMaps.cs

--- links-app/RouteMaps.cs
+++ links-app/RouteMaps.cs
@@ -6,20 +6,21 @@ namespace links_app
     {
         internal const string HomePageRoot = "https://www.michaelmeadows.com/";
         private const string GitHubRoot = "https://github.com/WalkingDisaster/";
+        private const string GoogleDrive = "https://docs.google.com/";
 
         public static IDictionary<string, string> Routes = new Dictionary<string, string>
         {
             {"/github", $"{GitHubRoot}"},
             {"/blog", $"{HomePageRoot}blog"},
+            
             {"/proj/edux", $"{GitHubRoot}edux/tree/stirtrek2017"},
-
             {"/st2017", $"{HomePageRoot}single-post/presentations/2017/stirtrek-resources"},
-            {"/pres/2017/stirtrek/slides", "https://docs.google.com/presentation/d/1T0iYRul6NdwM6GpE-jDuQBD13kFspxiXXXlwDQKOwpM/edit?usp=sharing"},
+            {"/pres/2017/stirtrek/slides", $"{GoogleDrive}presentation/d/1T0iYRul6NdwM6GpE-jDuQBD13kFspxiXXXlwDQKOwpM/edit?usp=sharing"},
             {"/pres/2017/stirtrek/edux", $"{GitHubRoot}edux/tree/stirtrek2017"},
-            {"/plus", "https://plus.google.com/u/0/+MichaelMeadowsAndYourMom"},
+            {"/+", "https://plus.google.com/+MichaelMeadows3f"},
             {"/cm2017-RAdAC", $"{HomePageRoot}single-post/2018/01/11/CodeMash-2018-RAdAC-Talk"},
             {"/cm2018-RAdAC", $"{HomePageRoot}single-post/2018/01/11/CodeMash-2018-RAdAC-Talk"},
-            {"/pres/2018/codemash/socket/slides", "https://docs.google.com/presentation/d/1EDPaVR0mAWGN1mU91vH3pRY5Xrz9MxYzZ0x48eu_LdE/edit?usp=sharing"},
+            {"/pres/2018/codemash/socket/slides", $"{GoogleDrive}presentation/d/1EDPaVR0mAWGN1mU91vH3pRY5Xrz9MxYzZ0x48eu_LdE/edit?usp=sharing"},
             {"/pres/2018/codemash/socket/code", $"{GitHubRoot}WalkingDisaster/edux/tree/CodeMash2018-edux"},
             {"/cm2018-socket", $"{HomePageRoot}single-post/2018/01/12/CodeMash-2018-SocketIO-Talk"},
             {"/sftodo", $"{HomePageRoot}service-fabric-to-do"},
