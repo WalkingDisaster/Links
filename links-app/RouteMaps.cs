bd1f9d8e3897470ab646e7fd7e7fa8f6eb33e379
Michael Meadows <michaelmeadows@gmail.com>
2018-09-03 16:33:51 -0400

    Update RouteMaps.cs

--- links-app/RouteMaps.cs
+++ links-app/RouteMaps.cs
@@ -17,14 +17,13 @@ namespace links_app
             {"/pres/2017/stirtrek/slides", "https://docs.google.com/presentation/d/1T0iYRul6NdwM6GpE-jDuQBD13kFspxiXXXlwDQKOwpM/edit?usp=sharing"},
             {"/pres/2017/stirtrek/edux", $"{GitHubRoot}edux/tree/stirtrek2017"},
             {"/plus", "https://plus.google.com/u/0/+MichaelMeadowsAndYourMom"},
-            {"/cm2017-RAdAC", "https://www.michaelmeadows.com/single-post/2018/01/11/CodeMash-2018-RAdAC-Talk"},
-            {"/cm2018-RAdAC", "https://www.michaelmeadows.com/single-post/2018/01/11/CodeMash-2018-RAdAC-Talk"},
+            {"/cm2017-RAdAC", $"{HomePageRoot}single-post/2018/01/11/CodeMash-2018-RAdAC-Talk"},
+            {"/cm2018-RAdAC", $"{HomePageRoot}single-post/2018/01/11/CodeMash-2018-RAdAC-Talk"},
             {"/pres/2018/codemash/socket/slides", "https://docs.google.com/presentation/d/1EDPaVR0mAWGN1mU91vH3pRY5Xrz9MxYzZ0x48eu_LdE/edit?usp=sharing"},
-            {"/pres/2018/codemash/socket/code", "https://github.com/WalkingDisaster/edux/tree/CodeMash2018-edux"},
-            {"/cm2018-socket", "https://www.michaelmeadows.com/single-post/2018/01/12/CodeMash-2018-SocketIO-Talk"},
-            {"/sftodo", "https://www.michaelmeadows.com/service-fabric-to-do"},
+            {"/pres/2018/codemash/socket/code", $"{GitHubRoot}WalkingDisaster/edux/tree/CodeMash2018-edux"},
+            {"/cm2018-socket", $"{HomePageRoot}single-post/2018/01/12/CodeMash-2018-SocketIO-Talk"},
+            {"/sftodo", $"{HomePageRoot}service-fabric-to-do"},
             {"/sessions", "https://sessionize.com/walkingdisaster"},
-            {"/blog", "https://www.michaelmeadows.com/blog"},
         };
     }
 }
