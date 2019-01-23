0005908b7a1f3411333daabcce5e557a716673f8
Michael Meadows <michaelmeadows@gmail.com>
2019-01-23 07:49:08 -0500

    Update RouteMaps.cs

--- links-app/RouteMaps.cs
+++ links-app/RouteMaps.cs
@@ -29,6 +29,7 @@ namespace links_app
             {"/cm2018-socket", $"{HomePageRoot}single-post/2018/01/12/CodeMash-2018-SocketIO-Talk"},
             {"/troll", "http://glench.com/hash/#diy.800.Max%5CnMax%5CnMax%5CnGreschl%5CnGreschl%5CnGreschl%5Cnis%5Cna%5CnGinger%5Cn%5CnGinger%5Cn%5CnGinger%5Cn%5CnGinger%5Cn"},
             {"/insight", $"https://insight.com"},
+            {"/483", $"https://photos.app.goo.gl/vbX4Yijhov1EuVok8"},
         };
     }
 }
