1cb9bd2e5877528c71ee65cb3e8d56c43d36e920
Michael Meadows <michaelmeadows@gmail.com>
2019-05-30 09:15:11 -0400

    Update RouteMaps.cs

--- links-app/RouteMaps.cs
+++ links-app/RouteMaps.cs
@@ -38,6 +38,7 @@ namespace links_app
             {"/pres/2019/coazure/cosmos", "https://www.michaelmeadows.com/single-post/2019/COAzure/CosmosDB"},
             
             {"/st19", $"{HomePageRoot}single-post/2019/stir-trek"},
+            {"/go/st19/v", "https://youtu.be/HghDNdXg9CU"},
             {"/pres/2019/stir-trek/slides", $"{GoogleDrive}presentation/d/1hxydNML8S0PSCzkvtz8V0Kgvsu2MKZ1qDtKaSPQKpRM/edit?usp=sharing"},
             {"/go/build19", "https://docs.google.com/presentation/d/1hBgb_e7YjmNO9dgD1efeGfhFUjX2jhq-tmkjqyipJCk/edit?usp=sharing"},
         };
